37711e7a5e63b583660518d34777c2994bdf3981
Imashi1 <fabianb175@gmail.com>
2023-02-02 17:35:46 -0300

    Base de datos
    Primera versión de base de datos, al menos se crea al hacer php artisan migrate

---
+++ database/migrations/2023_02_02_182417_inventario.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventario', function (Blueprint $table) {
+            $table->id('idinventario');
+            $table->string('codinventario');
+            $table->string('nombreinventario');
+            $table->string('fotoinventario');
+            $table->string('estadoinventario');
+            $table->string('informacioninventario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/migrations/2023_02_02_192246_material.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('material', function (Blueprint $table) {
+            $table->id('idinventariofk');
+            $table->foreign('idinventariofk')->references('idinventario')->on('inventario');
+            $table->integer('cantidadmaterial');
+            $table->string('patentevehiculo');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
